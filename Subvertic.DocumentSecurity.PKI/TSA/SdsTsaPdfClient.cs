ebc787e3cd33a850f3c0585edd0b5de71d8d8b52
Pedro Guzman <pedro@singularityresearch.net>
2016-04-12 17:16:30 -0600

    TimeStamp Authority Client Bug Fixes

---
+++ Subvertic.DocumentSecurity.PKI/TSA/SdsTsaPdfClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.log;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.security;
+
+namespace Subvertic.DocumentSecurity.PKI.TSA
+{
+    // ------------------------------------------------------------------------------------------------------------------------------------ //
+    // CLASS SDS TSA DEFAULT CLIENT                                                                                                         //
+    // ------------------------------------------------------------------------------------------------------------------------------------ //
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SdsTsaPdfClient
+    {
+
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // CLASS PRIVATE ATTRIBUTES                                                                                                         //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private ITSAClient tsaClient;
+
+        /// <summary>
+        ///     Stores the credentials to connect to the TimeStamp Authority
+        /// </summary>
+        private readonly TsaCredentials tsaCredentials;
+
+
+        /// <summary>
+        ///     Credentials Properties
+        /// </summary>
+        public TsaCredentials Credentials => tsaCredentials;
+
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // CONSTRUCTOR METHOD                                                                                                               //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="credentials"></param>
+        public SdsTsaPdfClient(TsaCredentials credentials)
+        {
+            if (credentials != null)
+            {
+                tsaCredentials = credentials;
+                tsaClient = new TSAClientBouncyCastle(Credentials.TsaUrl, Credentials.UserId, Credentials.Password, 3000, Credentials.TimeStampDigestAlg);
+            } // IF ENDS
+            else
+            {
+                throw new ArgumentNullException(nameof(credentials));
+            } // ELSE ENDS
+        } // CONSTRUCTOR METHOD ENDS ------------------------------------------------------------------------------------------------------ //
+
+
+
+    } // CLASS SDS TSA DEFAULT CLIENT ENDS ------------------------------------------------------------------------------------------------ //
+}
---
+++ Subvertic.DocumentSecurity.PKI/TSA/TsaCredentials.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subvertic.DocumentSecurity.PKI.TSA
+{
+
+    // ------------------------------------------------------------------------------------------------------------------------------------ //
+    // CLASS TSA CREDENTIALS                                                                                                                //
+    // ------------------------------------------------------------------------------------------------------------------------------------ //
+    /// <summary>
+    ///     This class is used to hold credentials required to use a Time Stamp Authority (TSA) Service
+    /// </summary>
+    public class TsaCredentials
+    {
+        public string UserId { set; get; }
+        public string Password { set; get; }
+        public string TsaUrl { set; get; }
+        public string TimeStampDigestAlg { set; get; }
+        public int TokenSizeEstimate { set; get; }
+
+
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // CONSTRUCTOR                                                                                                                      //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        /// <summary>
+        ///     Creates instances of TSA Credentials
+        /// </summary>
+        public TsaCredentials()
+        {
+            
+        } // DEFAULT CONSTRUCTOR ENDS ----------------------------------------------------------------------------------------------------- //
+        
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // CONSTRUCTOR METHOD                                                                                                               //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        /// <summary>
+        ///     Creates Instances of TSA Credentials with given parameters
+        /// </summary>
+        /// <param name="userId">Username or id used to connect to TSA</param>
+        /// <param name="password">Password associated to the TSA </param>
+        /// <param name="tsaUrl">Base Url of the TSA Service</param>
+        /// <param name="tsDigestAlg">Algorithm Used as TimeStamp Hash</param>
+        public TsaCredentials(string userId, string password, string tsaUrl, string tsDigestAlg, int tSzEstimate)
+        {
+            UserId = userId;
+            Password = password;
+            TsaUrl = tsaUrl;
+            TimeStampDigestAlg = tsDigestAlg;
+            TokenSizeEstimate = tSzEstimate;
+        } // CONSTRUCTOR METHOS ENDS ------------------------------------------------------------------------------------------------------ //
+
+    } // CLASS TSA CREDENTIALS ENDS ------------------------------------------------------------------------------------------------------- //
+}
