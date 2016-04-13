1fa86af29612eadd1a467a6f84c9810525ef990c
Pedro Guzman <pedro@singularityresearch.net>
2016-04-13 00:09:51 -0600

    Entropy Data Generator Added

---
+++ Subvertic.DocumentSecurity.Crypto/Entropy/RngRandomData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subvertic.DocumentSecurity.Crypto.Entropy
+{
+    // ------------------------------------------------------------------------------------------------------------------------------------ //
+    // CLASS RNG RANDOM DATA                                                                                                                //
+    // ------------------------------------------------------------------------------------------------------------------------------------ //
+    /// <summary>
+    ///     Contains a set of functions used to generate cryptographically secure random data
+    ///     <author>Pedro Guzmán (pedro@subvertic.com)</author>
+    /// </summary>
+    public class RngRandomData
+    {
+
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // CHARACTERS MAP                                                                                                                   //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        /// <summary>
+        ///     Valid characters map
+        /// </summary>
+        static readonly char[] Map = {
+
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_', '!',
+            '@', '#', '$', '%', '&', '*', '(', ')', '=', '+', '{', '}', '[',
+            ']', '|', '<', '>', '?'
+
+        }; // CHARACTER MAP ENDS ---------------------------------------------------------------------------------------------------------- //
+
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // METHOD NEXT  STRING                                                                                                              //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        /// <summary>
+        ///     Generates secure random strings with the given length
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string NextString(int length)
+        {
+            var randomSeed = new byte[length];   // Will store the entropy bytes used to generate the random bytes
+            var randString = new char[length];   // Will store the equivalent character translated using the Map
+
+            // Verify the length is > 0 to prevent crypto exceptions in RNG
+            if (length > 0)
+            {
+                using (var rng = new RNGCryptoServiceProvider())
+                {
+                    rng.GetBytes(randomSeed); // Fill the byte[] with entropy bytes
+                } // USING ENDS
+
+                for (var idx = 0; idx < randString.Length; ++idx)
+                {
+                    var relPosition = randomSeed[idx] % Map.Length;
+                    randString[idx] = Map[relPosition];
+                } // FOR ENDS 
+            } // IF ENDS
+            return new string(randString);
+        } // METHOD NEXT ENDS ------------------------------------------------------------------------------------------------------------- //
+
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // METHOD NEXT BYTES                                                                                                                //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        /// <summary>
+        ///     Generates a random byte array with variable length within a given threshold. 
+        /// </summary>
+        /// <param name="minLength">Minimun permited size fot the array</param>
+        /// <param name="maxLength">Maximun allowed size for the array</param>
+        /// <returns></returns>
+        public static byte[] NextBytes(int minLength, int maxLength)
+        {
+            var random = new Random();
+            var arrayLength = random.Next(minLength, maxLength);
+            var entropyArray = new byte[arrayLength];
+            var crng = new RNGCryptoServiceProvider();
+            crng.GetNonZeroBytes(entropyArray);
+            return entropyArray;
+        } // METHOD NEXT BYTES ENDS ------------------------------------------------------------------------------------------------------- //
+
+
+
+    } // CLASS RNG RANDOM DATA ENDS ------------------------------------------------------------------------------------------------------- //
+}
--- Subvertic.DocumentSecurity.Crypto/MessageDigests/Impl/SdsMessageDigestDefaultGenerator.cs
+++ Subvertic.DocumentSecurity.Crypto/MessageDigests/Impl/SdsMessageDigestDefaultGenerator.cs
@@ -7,14 +7,33 @@ using Subvertic.DocumentSecurity.Crypto.MessageDigests;
 
 namespace Subvertic.DocumentSecurity.Crypto.MessageDigests.Impl
 {
+
     // ------------------------------------------------------------------------------------------------------------------------------------ //
     // SDS MESSAGE DIGEST DEFAULT GENERATOR                                                                                                 //
     // ------------------------------------------------------------------------------------------------------------------------------------ //
     /// <summary>
+    ///     Used to compute hash values from byte arrays or string data. 
     ///     <author>Pedro Guzmán (pedro@subvertic.com)</author>
     /// </summary>
     public class SdsMessageDigestDefaultGenerator : ISdsMessageDigestGenerator
     {
+
+
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // CLASS CONSTANT DECLARATIONS                                                                                                      //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+
+        /// <summary>
+        ///     Minimun allowed length for salt bytes when used automatically salted hashes
+        /// </summary>
+        public const int MinSaltLength = 8;
+
+        /// <summary>
+        ///     Maximun allowed length for the salt bytes when used in automatically salted operations
+        /// </summary>
+        public const int MaxSaltLength = 24;
+
+
 
         public string Compute(string plainData, SdsMessageDigestAlgorithm algorithm)
         {
@@ -31,10 +50,20 @@ namespace Subvertic.DocumentSecurity.Crypto.MessageDigests.Impl
             throw new NotImplementedException();
         }
 
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        // METHOD COMPUTE                                                                                                                   //
+        // -------------------------------------------------------------------------------------------------------------------------------- //
+        /// <summary>
+        ///     Computes the hash value using given algorithm and provided data as a byte array.
+        /// </summary>
+        /// <param name="plainData"> Data to be hashed</param>
+        /// <param name="algorithm"> Digest algorithm to be used</param>
+        /// <param name="saltaBytes"> Salt bytes to be used in the operation </param>
+        /// <returns>byte[] with the computed value of the digest</returns>
         public byte[] Compute(byte[] plainData, SdsMessageDigestAlgorithm algorithm, byte[] saltaBytes)
         {
             throw new NotImplementedException();
-        }
+        } // METHOD CMOPUTE ENDS ---------------------------------------------------------------------------------------------------------- //
 
         public byte[] NewSalt(int maxSaltSize)
         {
